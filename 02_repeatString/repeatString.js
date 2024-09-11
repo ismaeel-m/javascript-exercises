230bf14e9d36ddc53f769b0d368edef12bd2364f
ismaeel-m <87246030+ismaeel-m@users.noreply.github.com>
2024-09-11 14:55:38 -0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(inputString, numberofLoops) {
+    let outputString = "";
 
+    if(numberofLoops >= 0){
+        for(let i = 0; i < numberofLoops; i++){
+            outputString += inputString;
+        }
+    } else{
+        outputString = "ERROR";
+    }
+
+    return outputString;
 };
 
 // Do not edit below this line
