3563e36c437b789f6db4bf9baf80f863cec05263
ismaeel-m <87246030+ismaeel-m@users.noreply.github.com>
2024-09-11 15:02:18 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let outputString = "";
 
+    // Take each character in reverse order and add it to output string
+    for(let i = inputString.length; i > 0; i--){
+        outputString += inputString.charAt(i- 1);
+    } 
+
+    return outputString;
 };
 
 // Do not edit below this line
